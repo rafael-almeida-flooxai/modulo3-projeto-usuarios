115605aecc8880e7974ff2a2a5aeee50b63513ef
rafokez <raphael.k.business@gmail.com>
2025-05-22 13:20:38 -0300

    feat: add classes

---
+++ models/User.js
@@ -0,0 +1,16 @@
+class User {
+
+    constructor(a, gender, birth, country, email, password, photo, admin){
+
+        this.name = a;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+
+    }
+
+}
