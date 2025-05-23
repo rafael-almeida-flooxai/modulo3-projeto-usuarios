8d2e8a985bc0b50b160c36f7f38dfc5ce1151d20
rafokez <raphael.k.business@gmail.com>
2025-05-23 11:43:47 -0300

    feat: adding admin true or false string

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -87,7 +87,7 @@ class UserController {
                 user[field.name] = field.checked;
 
             }
-                else {
+            else {
 
                 user[field.name] = field.value;
 
@@ -110,19 +110,21 @@ class UserController {
 
     addLine(dataUser) {
 
-        this.tableEl.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
                 <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${dataUser.birth}</td>
                 <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-        </tr>
     `;
+
+        this.tableEl.appendChild(tr);
 
     }
 
