8a1a12114f0ae98a01649cf4d6cbff9a0955ea5d
rafokez <raphael.k.business@gmail.com>
2025-05-23 14:40:18 -0300

    fix: fixed bugs in edit form

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,16 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
 
+    }
+
+    onEditC() {
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e => {
+
+            this.showPanelCreate();
+
+        });
     }
 
     onSubmit() {
@@ -141,15 +150,49 @@ class UserController {
                 <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
                 <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
     `;
+
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+
+            for (let name in json) {
+
+                let field = form.querySelector("[name=" + name.replace("_", "") + "]")
+
+
+
+                if (field) {
+                    
+                    if (field.type == 'file') continue;
+                    field.value = json[name];
+
+                }
+
+            }
+
+            this.showPanelUpdate();
+
+        })
 
         this.tableEl.appendChild(tr);
 
         this.updateCount();
 
+    }
+
+    showPanelCreate() {
+        document.querySelector("#box-user-create").style.display = "block";
+        document.querySelector("#box-user-update").style.display = "none";
+    }
+
+    showPanelUpdate() {
+        document.querySelector("#box-user-create").style.display = "none";
+        document.querySelector("#box-user-update").style.display = "block";
     }
 
     updateCount() {
